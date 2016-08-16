71189cbbf1f4c5a283f8494f1da6c4cde38c66ad
Raymond Lukanta <raymond_pluto@hotmail.com>
2016-08-16 10:07:41 +0700

    implement data binding for main page

--- app/src/main/java/com/lukanta/raymond/workshopschedulerapp/mainactivity/WorkshopAdapter.java
+++ app/src/main/java/com/lukanta/raymond/workshopschedulerapp/mainactivity/WorkshopAdapter.java
@@ -2,6 +2,8 @@ package com.lukanta.raymond.workshopschedulerapp.mainactivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +15,10 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import com.lukanta.raymond.workshopschedulerapp.BR;
 import com.lukanta.raymond.workshopschedulerapp.R;
 import com.lukanta.raymond.workshopschedulerapp.bookingpage.BookingActivity;
+import com.lukanta.raymond.workshopschedulerapp.databinding.RowWorkshopBinding;
 import com.lukanta.raymond.workshopschedulerapp.mappage.WorkshopDetailsActivity;
 import com.lukanta.raymond.workshopschedulerapp.mappage.WorkshopDetailsActivityFragment;
 import com.lukanta.raymond.workshopschedulerapp.model.Workshop;
@@ -37,72 +41,67 @@ public class WorkshopAdapter extends AbstractListAdapter<Workshop, WorkshopAdapt
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        return new ViewHolder(mInflater.inflate(R.layout.row_workshop, viewGroup, false));
+        RowWorkshopBinding binding = DataBindingUtil.inflate(LayoutInflater.from(viewGroup.getContext()), R.layout.row_workshop, viewGroup, false);
+        binding.setEventHandlers(this);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder routeViewHolder, int position) {
-        bind(routeViewHolder, position);
+        ViewDataBinding viewDataBinding = routeViewHolder.getViewDataBinding();
+        Workshop currentWorkshop = mData.get(position);
+        double distance = 0;
+        if (mCurrentLocation != null) {
+            distance = LatLngUtil.distance(mCurrentLocation, currentWorkshop.getWorkshopCoordinates()) / 1000;
+        }
+//        messagingViewHolder.workshopServiceDistanceTextView.setText(mContext.getString(R.string.distance, distance));
+        currentWorkshop.setDistance(distance);
+
+        viewDataBinding.setVariable(BR.workshop, currentWorkshop);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView workshopNameTextView;
-        private final TextView workshopServiceTyreTextView;
-        private final TextView workshopServiceOilTextView;
-        private final TextView workshopServiceBatteryTextView;
-        private final TextView workshopServiceDistanceTextView;
-        private final RatingBar workshopRatingBar;
-        private final ImageButton workshopMapImageButton;
-        private final Button workshopBookButton;
+        private ViewDataBinding mViewDataBinding;
 
-        public ViewHolder(View v) {
-            super(v);
-            workshopNameTextView = (TextView) v.findViewById(R.id.txt_workshop_name);
-            workshopRatingBar = (RatingBar) v.findViewById(R.id.rating_bar_workshop_rating);
-            workshopServiceTyreTextView = (TextView) v.findViewById(R.id.img_workshop_services_tyre);
-            workshopServiceOilTextView = (TextView) v.findViewById(R.id.img_workshop_services_oil);
-            workshopServiceBatteryTextView = (TextView) v.findViewById(R.id.img_workshop_services_battery);
-            workshopMapImageButton = (ImageButton) v.findViewById(R.id.btn_workshop_map);
-            workshopServiceDistanceTextView = (TextView) v.findViewById(R.id.txt_workshop_distance);
-            workshopBookButton = (Button)v.findViewById(R.id.btn_workshop_book);
+//        private final TextView workshopNameTextView;
+//        private final TextView workshopServiceTyreTextView;
+//        private final TextView workshopServiceOilTextView;
+//        private final TextView workshopServiceBatteryTextView;
+//        private final TextView workshopServiceDistanceTextView;
+//        private final RatingBar workshopRatingBar;
+//        private final ImageButton workshopMapImageButton;
+//        private final Button workshopBookButton;
+
+        public ViewHolder(ViewDataBinding viewDataBinding) {
+            super(viewDataBinding.getRoot());
+            mViewDataBinding = viewDataBinding;
+            mViewDataBinding.executePendingBindings();
+        }
+
+        public ViewDataBinding getViewDataBinding() {
+            return mViewDataBinding;
         }
     }
 
-    public void bind(ViewHolder messagingViewHolder, int position) {
-        final Workshop workshop = mData.get(position);
+//    public void bind(ViewHolder messagingViewHolder, int position) {
 
-        messagingViewHolder.workshopNameTextView.setText(workshop.getWorkshopName());
-        messagingViewHolder.workshopRatingBar.setRating(workshop.getCustomerRating());
-        messagingViewHolder.workshopMapImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(mContext, WorkshopDetailsActivity.class);
-                intent.putExtra(WorkshopDetailsActivityFragment.WORKSHOP_EXTRA, workshop);
-                mContext.startActivity(intent);
-            }
-        });
+//
+//        messagingViewHolder.workshopBookButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(mContext, BookingActivity.class);
+//                mContext.startActivity(intent);}
+//        });
+//    }
 
-        int tyreChangeVisibility = workshop.getTyreChange() == 0 ? View.GONE : View.VISIBLE;
-        messagingViewHolder.workshopServiceTyreTextView.setVisibility(tyreChangeVisibility);
+    public void onClickWorkshopMapImageButton(Workshop workshop) {
+        Intent intent = new Intent(mContext, WorkshopDetailsActivity.class);
+        intent.putExtra(WorkshopDetailsActivityFragment.WORKSHOP_EXTRA, workshop);
+        mContext.startActivity(intent);
+    }
 
-        int oilChangeVisibility = workshop.getOilChange() == 0 ? View.GONE : View.VISIBLE;
-        messagingViewHolder.workshopServiceOilTextView.setVisibility(oilChangeVisibility);
-
-        int batteryChangeVisibility = workshop.getBatteryChange() == 0 ? View.GONE : View.VISIBLE;
-        messagingViewHolder.workshopServiceBatteryTextView.setVisibility(batteryChangeVisibility);
-
-        double distance = 0;
-        if (mCurrentLocation != null) {
-            distance = LatLngUtil.distance(mCurrentLocation, workshop.getWorkshopCoordinates()) / 1000;
-        }
-        messagingViewHolder.workshopServiceDistanceTextView.setText(mContext.getString(R.string.distance, distance));
-        workshop.setDistance(distance);
-
-        messagingViewHolder.workshopBookButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(mContext, BookingActivity.class);
-                mContext.startActivity(intent);}
-        });
+    public void onClickWorkshopBookButton() {
+        Intent intent = new Intent(mContext, BookingActivity.class);
+        mContext.startActivity(intent);
     }
 }
